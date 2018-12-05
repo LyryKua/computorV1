cbf5b9fa9ebb5d58312cd1a61a0eb6485ff88f8b
Kyrylo <lyryk.ua@gmail.com>
2018-12-05 12:27:49 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-source venv/bin/activate
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "Equation: $line"
-    python computor.py "${line}"
-    echo ""
-done < "$1"
+if [[ $# -eq 1 ]];
+then
+    source venv/bin/activate
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        echo "Equation: $line"
+        python computor.py "${line}"
+        echo ""
+    done < "$1"
+else
+    echo "usage: ${0} source_file"
+fi
